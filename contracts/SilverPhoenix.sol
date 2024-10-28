2da4e7fe9ef100bc3767b8a95670a94bd8f0f7f6
BTC415 <alenschmitt415@gmail.com>
2024-10-28 15:50:33 +0000

    [add] added uniswap contract

--- contracts/SilverPhoenix.sol
+++ contracts/SilverPhoenix.sol
@@ -4,3 +4,7 @@ pragma solidity ^0.8.27;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
+
+contract SilverPhoenix is Context, Ownable, IERC20 {
+  
+}
